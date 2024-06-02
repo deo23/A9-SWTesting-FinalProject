81af93748209b02a9e60b0f4e16d42ecc926fc40
Muhammad Deo Audha Rizki <muhammad.deo.tif421@polban.ac.id>
2024-06-02 13:34:32 +0700

    feat: Add test script for Product Detail Page

--- src/test/java/swaglabs/pages/ProductDetailPage.java
+++ src/test/java/swaglabs/pages/ProductDetailPage.java
@@ -1,8 +1,8 @@
 package swaglabs.pages;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.By;
 
 public class ProductDetailPage {
     private WebDriver driver;
@@ -13,5 +13,21 @@ public class ProductDetailPage {
 
     public boolean isCorrectProductPageLoaded(String expectedUrl) {
         return driver.getCurrentUrl().equalsIgnoreCase(expectedUrl);
+    }
+
+    public void clickAddToCartButton() {
+        WebElement addToCartButton = driver.findElement(By.className("btn_inventory"));
+        addToCartButton.click();
+    }
+
+    public void clickBackToProductsButton() {
+        WebElement backButton = driver.findElement(By.className("inventory_details_back_button"));
+        backButton.click();
+    }
+
+    // Check if the product is added to the cart
+    public boolean isProductAddedToCart() {
+        WebElement cartItem = driver.findElement(By.className("shopping_cart_badge"));
+        return cartItem.isDisplayed();
     }
 }
